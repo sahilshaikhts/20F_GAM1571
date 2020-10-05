b8b5f7ab0b0e142587dca8bb0a0c09df09e6abdc
Sahil Shaikh <sahilshaikhts@gmail.com>
2020-10-05 10:17:33 -0400

    commit

--- PushToGit.bat
+++ PushToGit.bat
@@ -1,5 +1,5 @@
 git init
 git add -A
 git commit -m "commit"
-git set remote origin "https://github.com/sahilshaikhts/20F_GAM1571"
+git remote set origin "https://github.com/sahilshaikhts/20F_GAM1571"
 git push origin master
